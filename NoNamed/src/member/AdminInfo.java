e46b0c128696d4453014cd8c51697d29685c517e
Chanju Kim <62336546+DeApocalypse@users.noreply.github.com>
2021-09-21 17:44:41 +0900

    Add files via upload

---
+++ NoNamed/src/member/AdminInfo.java
@@ -0,0 +1,85 @@
+package member;
+
+public class AdminInfo {
+	
+	private String name;
+	private String date;
+	private String hotel;
+	private String people;
+	
+	public void printMemberInfo() {
+		System.out.println("이름 : "+name);
+		System.out.println("날짜 : "+date);
+		System.out.println("호텔 : "+hotel);
+		System.out.println("인원 수 : "+people);		
+	}
+
+	public AdminInfo(String name, String date, String hotel, String people) {
+		super();
+		this.name = name;
+		this.date = date;
+		this.hotel = hotel;
+		this.people = people;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(String hotel) {
+		this.hotel = hotel;
+	}
+
+	public String getPeople() {
+		return people;
+	}
+
+	public void setPeople(String people) {
+		this.people = people;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AdminInfo other = (AdminInfo) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
